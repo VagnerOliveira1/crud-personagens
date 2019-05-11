1dee4bcc408fba06cebaae51966e1083b2f02808
Vagner Oliveira <ti.vagner@gmail.com>
2019-05-11 07:55:33 -0300

    Cria Editar e Update personagem

--- app/controllers/personagens_controller.rb
+++ app/controllers/personagens_controller.rb
@@ -11,11 +11,32 @@ class PersonagensController < ApplicationController
     end
 
     def create
-        @personagem = Personagem.new(params[:personagem])
+        @personagem = Personagem.new(personagem_params)
         if @personagem.save
             redirect_to @personagem, :notice =>' Personagem cadastrado com sucesso!!!'
         else
             render :new
+        end
+    end
+
+    def personagem_params
+        params.
+            require(:personagem).
+            permit(:nome, :forca, :resistencia, :agilidade,
+                 :destreza)
+    end
+
+    def edit
+        @personagem = Personagem.find(params[:id])
+    end
+
+    def update
+        @personagem = Personagem.find(params[:id])
+        if @personagem.update(personagem_params)
+            redirect_to @personagem,
+                notice:'Personagem atualizado!!!'
+        else
+            render action: :edit
         end
     end
 
